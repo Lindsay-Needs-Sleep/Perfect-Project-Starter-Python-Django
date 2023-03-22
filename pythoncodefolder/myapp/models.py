f281f4ecfdc7d592111e06c59ae71b8466e706af
Lindsay-Needs-Sleep <lindsay-needs-sleep@outlook.com>
2023-03-21 21:12:14 -0600

    styling

--- pythoncodefolder/myapp/models.py
+++ pythoncodefolder/myapp/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 
 # Create your models here.
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -21,5 +22,6 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
     def __str__(self):
         return self.choice_text
