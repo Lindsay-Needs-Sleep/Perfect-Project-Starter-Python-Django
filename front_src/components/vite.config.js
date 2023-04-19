f72caca9e4b71d5d03e11606c7f4e4900ecbc028
Lindsay-Needs-Sleep <lindsay-needs-sleep@outlook.com>
2023-04-19 15:28:14 -0600

    front - get watching working for server and re-building the bundle. (vite's watch options don't seem to work for re-building for some reason, so just use nodemon... has something to do with docker/wsl/files on windows)

--- front_src/components/vite.config.js
+++ front_src/components/vite.config.js
@@ -1,17 +1,27 @@
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    publicDir: '../public/',
-    build: {
-        outDir: '../public/bundles/',
-        copyPublicDir: false,
-        lib: {
-            entry: 'src/my-element.js',
-            formats: ['es'],
+export default defineConfig(({ command, mode, ssrBuild }) => {
+    const prod = mode == 'production';
+    return {
+        publicDir: '../public/',
+        build: {
+            outDir: '../public/bundles/',
+            copyPublicDir: false,
+            minify: prod,
+            sourcemap: prod ? 'hidden' : true,
+            lib: {
+                entry: 'src/my-element.js',
+                formats: ['es'],
+            },
+            rollupOptions: {
+                external: /^lit/,
+            }
         },
-        rollupOptions: {
-            external: /^lit/,
-        },
-    },
+        server: {
+            watch: {
+              usePolling: true
+            }
+          }
+    }
 })
--- frontend-start.sh
+++ frontend-start.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 # usage: ./frontend-start.sh <mode>
 # mode: default - dev server
+#       p - production build
 #       i - interactive
 
 EXEC="docker compose exec -it front"
 
 case $1 in
     i) COMMAND=(bash);;
-    *) COMMAND=(npm run dev);;
+    p) COMMAND=(bash -c "
+        npm install --omit=dev &&
+        npm run build
+        ");;
+    *) COMMAND=(bash -c"
+        npm install &&
+        npm run dev
+        ");;
 esac
 
 # Ensure the developement containers are running (python and front are just sleeping)
